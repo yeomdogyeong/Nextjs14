6035657ab8a4968f40173fa959e0631ca0e6198c
dogyeong <82639552+yeomdogyeong@users.noreply.github.com>
2024-04-05 19:06:08 +0900

    [feat]loading컴포넌트 적용

--- app/movies/page.tsx
+++ app/movies/page.tsx
@@ -1,24 +1,19 @@
-"use client";
-import React, { useEffect, useState } from "react";
+import { url } from "inspector";
 
-function Movies() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
-    const res = await fetch(
-      "https://nomad-movies.nomadcoders.workers.dev/movies"
-    );
-    const json = await res.json();
-    console.log(res);
-    setMovies(json);
-    setIsLoading(false);
-  };
-  useEffect(() => {
-    getMovies();
-  }, []);
-  return (
-    <div>movies : {isLoading ? "loading...." : JSON.stringify(movies)}</div>
-  );
+export const metadata = {
+  title: "Movie",
+};
+const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
+
+async function getMovies() {
+  await new Promise((el) => setTimeout(el, 5000));
+  return fetch(URL).then((response) => response.json());
 }
 
-export default Movies;
+export default async function Movies() {
+  const movies = await getMovies();
+
+  return <div>{JSON.stringify(movies)}</div>;
+}
+
+//서버에서 Data를 Fetch할 때 loading 파일을 제공해주면 해당 로딩페이지가 적용된다.
