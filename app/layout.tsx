a1890f266e15adf2e3d3d2e785870bbc33fd04e4
dogyeong <82639552+yeomdogyeong@users.noreply.github.com>
2024-04-15 19:11:27 +0900

    [add]style 폴더 생성 후 global.css이동

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import "./globals.css";
+import "../styles/global.css";
 import { Navigation } from "../components/navigation";
 
 export const metadata: Metadata = {
